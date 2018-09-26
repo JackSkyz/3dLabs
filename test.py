9a97f1df859c6019b1610445545f4f7a72b48cb8
JackSkyz <31199353+JackSkyz@users.noreply.github.com>
2018-09-26 19:36:23 -0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,92 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+
+root = Builder.load_string('''
+#:import NoTransition kivy.uix.screenmanager.NoTransition
+#:set spac 20
+#:set pad 10
+#:set x_screen 480
+#:set y_screen 800
+
+BoxLayout:
+    orientation: 'vertical'
+    canvas.before:
+        Color:
+            rgba: (1,1,1,1)
+        Rectangle:
+            pos: self.pos
+            size: self.size
+            source: './imagenes/background.jpg'
+            
+            
+        Color:
+            rgba: (0.25,0.25,0.25,0.7)
+        Rectangle:
+            pos: (0, x_screen - 30)
+            size: self.size
+    
+    BoxLayout:
+        orientation: 'vertical'
+
+        ScreenManager:
+            transition: NoTransition()
+            size_hint: 1, .8
+            id: sm
+            Screen:
+                name: 'game'
+                Label:
+                    text: 'Game'
+
+            Screen:
+                name: 'settings'
+                Label:
+                    text: 'Settings' 
+    
+    BoxLayout:
+        
+        CheckBox:
+            group: 'buttons'
+            text: "Settings"
+            on_state: sm.current = 'game'
+            state: 'down'
+            allow_no_selection: False
+        
+        CheckBox:
+            group: 'buttons'
+            text: "Settings"
+            on_state: sm.current = 'settings'
+            allow_no_selection: False
+            
+        CheckBox:
+            group: 'buttons'
+            text: "Settings"
+            on_state: sm.current = 'settings'
+            allow_no_selection: False
+            
+        CheckBox:
+            group: 'buttons'
+            text: "Settings"
+            on_state: sm.current = 'settings'
+            allow_no_selection: False
+            
+        CheckBox:
+            group: 'buttons'
+            text: "Settings"
+            on_state: sm.current = 'settings'
+            allow_no_selection: False
+        
+
+
+    
+
+''')
+
+
+class MyApp(App):
+
+    def build(self):
+        return root
+
+
+MyApp().run()
