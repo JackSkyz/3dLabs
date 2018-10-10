c1e32a039c94d9442554873194d13ea0e3a2dfac
JackSkyz <31199353+JackSkyz@users.noreply.github.com>
2018-10-10 14:49:19 -0300

    Add files via upload

--- labsApp.py
+++ labsApp.py
@@ -825,7 +825,7 @@ class MyApp(App):
         Clock.schedule_interval(self.My.update_temperature, 0.5)
         Clock.schedule_interval(self.My.check_usb, 0.5)
         
-        Clock.schedule_interval(self.My.ArduinoRead, 1/25)
+        Clock.schedule_interval(self.My.ArduinoRead, 1/100)
 #        Clock.schedule_interval(self.My.ArduinoWrite, 1/25)
         Thread(target=self.My.ArduinoWrite).start()
         Clock.schedule_interval(self.My.requestTemperature, .25)
